9fe7c8408f7109242953fe2673875c10b92c02e5
chrisevans--laptop <evansc@mssm.org>
2017-04-07 14:58:15 -0400

    working on sessions

--- crawler.js
+++ crawler.js
@@ -2,18 +2,20 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 var express = require('express');
-var sessions = require('client-sessions');
-
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-var bodyParser = require('body-parser');
+app.use(session({secret: 'ssshhhhh'}));
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+var sess;
 
 var questions = [];
 var tempQuestions = [];
@@ -155,28 +157,18 @@ function parseTitle(title){
 
 
 
-app.use(sessions({
-  cookieName: 'mySession', // cookie name dictates the key name added to the request object
-  secret: 'blargadeeblargblarg', // should be a large unguessable string
-  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
-  activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-}));
-
-app.use(function(req, res, next) {
-  if (req.mySession.seenyou) {
-    res.setHeader('X-Seen-You', 'true');
-  } else {
-    // setting a property will automatically cause a Set-Cookie response
-    // to be sent
-    req.mySession.seenyou = true;
-    res.setHeader('X-Seen-You', 'false');
-  }
-});
-
-
-
 var qNum = 0;  //temporary tracker variable
 app.use(express.static('public'));
+
+app.get("/", function(req, res){
+  sess = req.session;
+  if(sess.visited){
+
+  }else{
+    sess.visited = true;
+  }
+  res.render("index.html");
+});
 
 app.get("/qs/:qid", function(req, res){
   res.send(tempQuestions[req.params.qid]);
